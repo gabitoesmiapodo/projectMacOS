40dae1951285f4954ac7563708a93a0d037e9d43
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2026-03-14 00:52:45 -0300

    fix: remove broken GUID guard from ProjectMPrefsParent.h

--- mac/ProjectMPrefsParent.h
+++ mac/ProjectMPrefsParent.h
@@ -1,21 +1,9 @@
 #pragma once
 #import <Cocoa/Cocoa.h>
 
-// GUID is defined in pfc/pfc-lite.h (foobar2000 SDK). Mirror the definition here
-// under the same guard so this header is self-contained for IDE analysis.
-#ifndef GUID_DEFINED
-#define GUID_DEFINED
-struct GUID {
-    uint32_t Data1;
-    uint16_t Data2;
-    uint16_t Data3;
-    uint8_t  Data4[8];
-} __attribute__((packed));
-#endif
-
 // kPrefsParentGUID: GUID for the "projectMacOS" parent preferences_page node.
 // All five section pages return this from get_parent_guid().
-// Defined in ProjectMPreferences.mm.
+// Defined in ProjectMPreferences.mm. GUID is from the foobar2000 SDK (stdafx.h).
 extern const GUID kPrefsParentGUID;
 
 // PMPrefsHelpers: layout utilities shared across all five section view controllers.
